e6a502c0c0a4e3f5decd25756b175b687a3c25ae
Josphat Munene <munenejosphat17@gmail.com>
2025-03-16 13:49:22 +0300

    Show all notes

---
+++ resources/views/note/index.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+    <div class="note-container py-12">
+        <a href="{{route('notes.create')}}" class="new-note-btn">
+            New Note
+        </a>
+        <div class="notes">
+            @foreach ($notes as $note )
+                <div class="note">
+                    <div class="note-body">
+                        {{Str::words($note->note, 30)}}
+                    </div>
+                    <div class="note-buttons">
+                        <a href="{{route('notes.show', $note)}}" class="note-edit-button">View</a>
+                        <a href="{{route('notes.edit', $note)}}" class="note-edit-button">Edit</a>
+                        <form action="{{route('notes.destroy', $note)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="note-delete-button">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
+                
+        </div>
+    </div>
+</x-layout>
